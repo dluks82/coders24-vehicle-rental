b35497698ab6e82d7cafe9893f22e34517be7a26
Diogo Oliveira <dluks82@gmail.com>
2024-11-26 16:23:53 -0300

    :wrench: chore: Initial commit

---
+++ backend/src/main/java/dev/dluks/rental/RentalApplication.java
@@ -0,0 +1,15 @@
+package dev.dluks.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class RentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalApplication.class, args);
+    }
+
+}
