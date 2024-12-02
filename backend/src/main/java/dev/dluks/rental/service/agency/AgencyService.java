f400f0eba20e7adf9c64009b735009afe2c8d7f7
Isaque Menezes <isaquesantos1517@gmail.com>
2024-12-02 20:33:06 -0300

    refactor: enhance AgencyService with additional methods and update logic
    - Add `getAgencyById` to retrieve agency by ID with exception handling.
    - Implement `findByDocument` and `findByName` methods with proper exception handling.
    - Introduce `updateAgency` method with flexible field updating using lambdas.
    - Use `@RequiredArgsConstructor` to simplify constructor injection.

--- backend/src/main/java/dev/dluks/rental/service/agency/AgencyService.java
+++ backend/src/main/java/dev/dluks/rental/service/agency/AgencyService.java
@@ -1,16 +1,23 @@
 package dev.dluks.rental.service.agency;
 
+import dev.dluks.rental.exception.AgencyNotFoundException;
 import dev.dluks.rental.model.agency.Agency;
 import dev.dluks.rental.repository.AgencyRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+import java.util.function.Consumer;
+
 @Service
+@RequiredArgsConstructor
 public class AgencyService {
 
     private final AgencyRepository agencyRepository;
 
-    public AgencyService(AgencyRepository agencyRepository) {
-        this.agencyRepository = agencyRepository;
+    public Agency getAgencyById(UUID id) {
+        return agencyRepository.findById(id)
+                .orElseThrow(() -> new AgencyNotFoundException(id.toString() + " not found."));
     }
 
     public Agency create(CreateAgencyRequest dto) {
@@ -23,12 +30,32 @@ public class AgencyService {
         return agencyRepository.save(agency);
     }
 
-    public void findByDocument() {
+    public Agency findByDocument(String document) {
+        return agencyRepository.findByDocument(document)
+                .orElseThrow(() -> new AgencyNotFoundException("Agency by document " + document + " not found"));
     }
 
-    public void findByName() {
+    public Agency findByName(String name) {
+        return agencyRepository.findByNameIgnoreCase(name)
+                .orElseThrow(() -> new AgencyNotFoundException("Agency by name " + name + " not found"));
     }
 
-    public void updateAgency() {
+    private void updateField(Consumer<String> setter, String value) {
+        if (value != null && !value.isEmpty()) {
+            setter.accept(value);
+        }
     }
+
+    public Agency updateAgency(UUID id, UpdateAgencyRequest updateAgencyRequest) {
+       Agency agency = getAgencyById(id);
+
+       updateField(agency::setName, updateAgencyRequest.getName());
+       updateField(agency::setDocument, updateAgencyRequest.getDocument());
+       updateField(agency::setPhone, updateAgencyRequest.getPhone());
+       updateField(agency::setEmail, updateAgencyRequest.getEmail());
+
+       agencyRepository.save(agency);
+       return agency;
+    }
+
 }
