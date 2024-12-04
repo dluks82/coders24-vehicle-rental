1c59f47d935dbca5df4be1a368711183c2a0da58
Rômulo Domingos <romulo.domingos11@gmail.com>
2024-12-04 15:44:31 -0300

    :rotating_light: Created Customer Service unit test class

---
+++ backend/src/test/java/dev/dluks/rental/service/customer/CustomerServiceImplTest.java
@@ -0,0 +1,37 @@
+package dev.dluks.rental.service.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createCustomer() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void findCustomerById() {
+    }
+
+    @Test
+    void findCustomerByDocument() {
+    }
+
+    @Test
+    void findAllCustomers() {
+    }
+
+    @Test
+    void findCustomerByName() {
+    }
+}
