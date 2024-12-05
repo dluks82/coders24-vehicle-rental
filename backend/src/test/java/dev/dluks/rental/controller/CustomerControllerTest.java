6ef1e061d344c8b3002a1a92fb76674857735197
Rômulo Domingos <romulo.domingos11@gmail.com>
2024-12-05 15:20:14 -0300

    :white_check_mark: Created Test createCustomer in CustomerControllerTest.java

--- backend/src/test/java/dev/dluks/rental/controller/CustomerControllerTest.java
+++ backend/src/test/java/dev/dluks/rental/controller/CustomerControllerTest.java
@@ -1,22 +1,61 @@
 package dev.dluks.rental.controller;
 
+import dev.dluks.rental.model.customer.Customer;
+import dev.dluks.rental.model.customer.CustomerType;
+import dev.dluks.rental.service.customer.CustomerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class CustomerControllerTest {
 
+    @InjectMocks
+    private CustomerController customerController;
+    @Mock
+    private CustomerService customerService;
+
+    private Customer customer;
+
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+         customer = Customer.builder()
+                 .name("John Doe")
+                .document("00146729013")
+                .type(CustomerType.INDIVIDUAL)
+                .phone("12345678901")
+                .email("j@j.com")
+                .build();
+
     }
 
     @Test
     void createCustomer() {
+
+        when(customerService.createCustomer(customer)).thenReturn(customer);
+
+        ResponseEntity<Customer> response = customerController.createCustomer(customer);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(customer, response.getBody());
+        verify(customerService).createCustomer(customer);
     }
 
     @Test
     void updateCustomer() {
+
     }
 
     @Test
