3f88c14c90a13654559b7b3aefe7330effba971e
Rômulo Domingos <romulo.domingos11@gmail.com>
2024-12-05 14:22:32 -0300

    modify test main

--- backend/src/test/java/dev/dluks/rental/RentalApplicationTest.java
+++ backend/src/test/java/dev/dluks/rental/RentalApplicationTest.java
@@ -3,10 +3,12 @@ package dev.dluks.rental;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RentalApplicationTest {
 
     @Test
