df12f50fc503012e25cbbfa25bfd23ea8ffb3c8e
Rômulo Domingos <romulo.domingos11@gmail.com>
2024-12-05 15:02:51 -0300

    :rotating_light: Created CustomerControllerTest.java

---
+++ backend/src/test/java/dev/dluks/rental/controller/CustomerControllerTest.java
@@ -0,0 +1,37 @@
+package dev.dluks.rental.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createCustomer() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void findCustomerById() {
+    }
+
+    @Test
+    void findCustomerByDocument() {
+    }
+
+    @Test
+    void findAllCustomers() {
+    }
+
+    @Test
+    void findCustomerByName() {
+    }
+}
